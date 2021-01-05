81fae5b2b31f14c43f566d7f5a5f730d34554845
Thomas Henry <tomjohnhenry@gmail.com>
2021-01-05 08:20:16 -0600

    add initial gauge chart code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -77,9 +77,11 @@ function createBarChart(selectValue) {
 }     
 
 function createBubbleChart(valueSelect) {
+
   var filterValue3 = data.samples.filter(value => value.id == valueSelect);
   var otuId = filterValue3.map(value => value.otu_ids);
   otuId = otuId[0];
+
   var sampValues = filterValue3.map(value => value.sample_values);
   sampValues = sampValues[0];
 
@@ -118,6 +120,45 @@ function createBubbleChart(valueSelect) {
   Plotly.newPlot("bubble", [trace], layout);
 }
 
-function createGaugeChart(valueSelect) {
 
+function createGuageChart(valueSelect) {
+
+  var filterValue = data.metadata.filter(value => value.id == valueSelect);
+  var weeklyFreq = filterValue[0].wfreq;
+
+  var data = [
+    {
+      type: "indicator",
+      mode: "gauge+number+delta",
+      value: 420,
+      title: { text: "Speed", font: { size: 24 } },
+      delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+      gauge: {
+        axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
+        bar: { color: "darkblue" },
+        bgcolor: "white",
+        borderwidth: 2,
+        bordercolor: "gray",
+        steps: [
+          { range: [0, 250], color: "cyan" },
+          { range: [250, 400], color: "royalblue" }
+        ],
+        threshold: {
+          line: { color: "red", width: 4 },
+          thickness: 0.75,
+          value: weeklyFreq
+        }
+      }
+    }
+    ];
+  
+  var layout = {
+    width: 500,
+    height: 400,
+    margin: { t: 25, r: 25, l: 25, b: 25 },
+    paper_bgcolor: "lavender",
+    font: { color: "darkblue", family: "Arial" }
+  };
+  
+  Plotly.newPlot('gauge', data, layout);
 }
