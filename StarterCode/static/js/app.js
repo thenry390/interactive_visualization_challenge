61e18dbfb85c4af67c763cf3122155ee29cb5ab4
Thomas Henry <tomjohnhenry@gmail.com>
2021-01-04 18:52:35 -0600

    refactoring code to be more streamlined

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -36,15 +36,22 @@ function createBarChart(selectValue) {
   // for the samples, grab the OTU IDs
   var otuIds = filterValue.map(value => value.otu_ids);
   // call function getOtuids passing the first 10 values to get the list of 10 OTU IDs
-  otuIds = getOtuids(otuIds[0].slice(0, 10)).reverse();
+  //otuIds = getOtuids(otuIds[0].slice(0, 10)).reverse();
+  var names = otuIds[0].slice(0, 10);
+  var listOfOtuids = [];
+
+  for (var i = 0; i < names.length; i++) {
+    listOfOtuids.push(`OTU ${names[i]}`);
+  }
+  listOfOtuids.reverse();
+
   // grab the sample values from samples
   var samples = filterValue.map(value => value.sample_values);
   // grab the first 10 OTU IDs
   sampleCounts = samples[0].slice(0, 10).reverse();
   // grab the OTU Labels from the samples
   var otuLabels = filterValue.map(value => value.otu_labels);
-  // call function getBackNames passing the first 10 OTU Labels to get the list of 10 bacteria names
- 
+  
   var otuLabels = (otuLabels[0]).slice(0, 10).reverse();
 
   var trace = {
@@ -52,7 +59,7 @@ function createBarChart(selectValue) {
     // x axis
     x: sampleCounts,
     // Y axis
-    y: otuIds,
+    y: listOfOtuids,
     // hover text
     //text: otuLabels,
     type: "bar",
@@ -72,39 +79,16 @@ function bubbleChart(valueSelect) {
   var filterValue3 = data.samples.filter(value => value.id == valueSelect);
   var otuId = filterValue3.map(value => value.otu_ids);
   otuId = otuId[0];
-  var sampValues = filterValue3.map(v => v.sample_values);
+  var sampValues = filterValue3.map(value => value.sample_values);
   sampValues = sampValues[0];
 
-  var outLabel = filterValue3.map(v => v.otu_labels);
-  outLabel = getBactNames(outLabel[0]);
-
-  var trace = {
-    x: otuId,
-    y: sampValues,
-    mode: "markers",
-    marker: {
-      color: otuId,
-      size: sampValues
-    },
-    text: outLabel
-  };
-
-  var layout = {
-      xaxis: { title: "OTU ID" }
-  };
-
-  Plotly.newPlot("bubble", [trace], layout);
-}
-// function to return the name of the bacteria.
-// if an array value has more than one name, it will consider the last name of the value
-// return just the 10 first values of the result
-function getBactNames(names) {
-
+  var outLabel = filterValue3.map(value => value.otu_labels);
+  //outLabel = getBactNames(outLabel[0]);
   var listOfBacts = [];
 
-  for (var i = 0; i < names.length; i++) {
+  for (var i = 0; i < outLabel[0].length; i++) {
 
-    var stringName = names[i].toString();
+    var stringName = outLabel.toString();
     var splitValue = stringName.split(";");
 
     if (splitValue.length = 1) {
@@ -115,16 +99,20 @@ function getBactNames(names) {
     }
   }
 
-  return listOfBacts;
-}
+  var trace = {
+    x: otuId,
+    y: sampValues,
+    mode: "markers",
+    marker: {
+      color: otuId,
+      size: sampValues
+    },
+    text: listOfBacts
+  };
 
-function getOtuids(names) {
+  var layout = {
+      xaxis: { title: "OTU ID" }
+  };
 
-  var listOfOtuids = [];
-
-  for (var i = 0; i < names.length; i++) {
-    listOfOtuids.push(`OTU ${names[i]}`);
-  }
-
-  return listOfOtuids;
+  Plotly.newPlot("bubble", [trace], layout);
 }
